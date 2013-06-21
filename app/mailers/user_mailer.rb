6fa4a4ff7fb18025a96b5c0ae459b8bed171f739
vinay <vbvyas@gmail.com>
2013-06-21 01:56:33 -0700

    Change reset email to be sent from no-reply@closefeed.com

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "external@closefeed.com"
+  default from: "no-reply@closefeed.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
