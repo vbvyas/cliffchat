b2dbeeeaa9d2f6ed1535684974a66c376b5a0836
vinay <vbvyas@gmail.com>
2013-09-02 00:22:15 -0700

    Tell user if the email doesn't exist.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,8 +1,12 @@
 class PasswordResetsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
-    user.send_password_reset if user
-    redirect_to root_path, notice: "Email sent with password reset instructions."
+    if user
+      user.send_password_reset
+      redirect_to root_path, notice: "Email sent with password reset instructions."
+    else
+      redirect_to :back, flash: { error: "Email not found: #{params[:email]}. Try another email." }
+    end
   end
 
   def edit
